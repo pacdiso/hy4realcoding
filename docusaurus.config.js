c4c4caee217d5da9b9a73418043f63ba78f75544
pacdiso <92020680+pacdiso@users.noreply.github.com>
2024-08-26 14:55:40 +0800

    docuconfig fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,12 +16,14 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/hy4realcoding/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'pacdiso', // Usually your GitHub org/user name.
+  projectName: 'hy4realcoding', // Usually your repo name.
+
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
